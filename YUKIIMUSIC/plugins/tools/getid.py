1c00f980e933544aaf02873d8f5a0bb416322e65
HellfireDevs <devshellfire@gmail.com>
2026-04-09 15:03:43 +0530

    Update getid.py

--- YUKIIMUSIC/plugins/tools/getid.py
+++ YUKIIMUSIC/plugins/tools/getid.py
@@ -2,8 +2,9 @@ from pyrogram import filters
 from YUKIIMUSIC import app
 from YUKIIMUSIC.misc import SUDOERS
 
-@app.on_message(filters.video & filters.private & filters.user(SUDOERS))
+@app.on_message(filters.video & filters.private & SUDOERS)
 async def get_video_id(client, message):
     # Jaise hi tu video bhejega, bot uski file_id reply kar dega
     file_id = message.video.file_id
     await message.reply_text(f"**Yᴇ ʀᴀʜɪ Tᴇʀɪ Fɪʟᴇ ID:**\n\n`{file_id}`")
+    
