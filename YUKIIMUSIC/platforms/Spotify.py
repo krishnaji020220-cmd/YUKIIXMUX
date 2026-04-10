7819876204a383271587b5fdd505da85e39493e1
Itachiplub-cloud <itachiplub@gmail.comm>
2026-04-10 04:42:53 +0000

    v9.5 - Added HellfireDevs Copyright 🛡️

--- YUKIIMUSIC/platforms/Spotify.py
+++ YUKIIMUSIC/platforms/Spotify.py
@@ -1,3 +1,24 @@
+# Copyright (c) 2025 @SUDEEPBOTS <HellfireDevs>
+# Location: delhi,noida
+#
+# All rights reserved.
+#
+# This code is the intellectual SUDEEPBOTS.
+# You are not allowed to copy, modify, redistribute, or use this
+# code for commercial or personal projects without explicit permission.
+#
+# Allowed:
+# - Forking for personal learning
+# - Submitting improvements via pull requests
+#
+# Not Allowed:
+# - Claiming this code as your own
+# - Re-uploading without credit or permission
+# - Selling or using commercially
+#
+# Contact for permissions:
+# Email: sudeepgithub@gmail.com
+
 import YUKIIMUSIC.yuki_guard
 import re
 
